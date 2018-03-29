3ca6d844a23cfef314626748fd9d0bd655415a7e
arjen <Arjen.bovee@live.nl>
2018-03-29 13:11:18 +0200

    -initial commit with all files

---
+++ src/Models/TicTacToeDataset.java
@@ -0,0 +1,10 @@
+package Models;
+
+public class TicTacToeDataset extends Dataset {
+    public TicTacToeDataset()
+    {
+        gameData = new int[9];
+        gameType = GameType.Tictactoe;
+        aI = false;
+    }
+}
