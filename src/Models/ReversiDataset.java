a41c6e8a28477a3f1b5fec3d0b720e0717956807
arjen <Arjen.bovee@live.nl>
2018-04-04 15:47:05 +0200

    -fixed allot of reversi logic -made the first version work

--- src/Models/ReversiDataset.java
+++ src/Models/ReversiDataset.java
@@ -6,11 +6,9 @@ public class ReversiDataset extends  Dataset {
         gameData = new int[64];
         gameType = GameType.Reversi;
 
-        gameData[27] = 1;
-        gameData[28] = 2;
-        gameData[35] = 2;
-        gameData[36] = 1;
-
-        AI = false;
+        gameData[27] = 2;
+        gameData[28] = 1;
+        gameData[35] = 1;
+        gameData[36] = 2;
     }
 }
