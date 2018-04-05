d3574c36924d26b5c96fe378bf06d2dd44de9c10
TheElmo <jelmer.internet@xs4all.nl>
2018-04-05 14:57:43 +0200

    Basic AI for tic tac toe

--- src/AI/AI.java
+++ src/AI/AI.java
@@ -1,5 +1,6 @@
 package AI;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class AI {
@@ -9,10 +10,23 @@ public class AI {
 
     public int makeMove(int[] pMoves) {
         System.out.println("AI is making random move");
-        System.out.println(pMoves);
-        int move;
         Random random = new Random();
-        move = random.nextInt(pMoves.length-1);
+        ArrayList<Integer> moves = new ArrayList<>(pMoves.length);
+        for(int i = 0; i < pMoves.length; i++) {
+            if(pMoves[i] == 1) {
+                System.out.println("Empty cell found: " + i);
+                moves.add(i);
+            }
+        }
+        moves.trimToSize();
+        for(int i = 0; i < moves.size(); i++) {
+            System.out.println("in moves: " + moves.get(i));
+        }
+        System.out.println(moves.size());
+        int index = random.nextInt(moves.size());
+        System.out.println("Random index: " + index);
+        int move = moves.get(index);
+
         System.out.println("Made up move: " + move);
         return move;
     }
