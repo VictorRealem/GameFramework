0f89b1d54f5dc896e8d4fee8cf244a6afab8063c
Wouter Kurvers <WKurvers11@gmail.com>
2018-04-08 10:41:44 +0200

    Merge branch 'master' of https://github.com/wkurvers/GameFramework
    # Conflicts:
    #	src/Controllers/TicTacToeController.java

--- src/Views/GameBoardView.java
+++ src/Views/GameBoardView.java
@@ -47,7 +47,6 @@ public class GameBoardView{
         borderPane.setCenter(centerPane);
         borderPane.setLeft(new LeftPane(controller));
         borderPane.setRight(new RightPane(controller));
-
         return new Scene(borderPane,1000,900);
     }
 }
